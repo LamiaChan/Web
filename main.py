d42fb52f847e0ef4067f122c8a41c612fd895aca
mar4elkin <googlcraftlol@gmail.com>
2019-10-29 23:26:03 +0300

    feat(main): работа с картинками

--- main.py
+++ main.py
@@ -17,21 +17,31 @@ def get_list_presentation(html):
 def get_manga(html):
     soup = BeautifulSoup(html, 'lxml')
 
+    url_array = []
+
     for option in soup.find_all('option'):
         print (('value: {}, text: {}').format(option['value'], option.text))
-    
-    
-    '''for i in soup.find('select', id='chapterSelectorSelect'):
-        print(i['value'])'''
+        url_array.append(option['value'][:-5])
+
+    for j in range(0, len(url_array)):
+        print(url_array[j])
+
+
+def get_picks(html):
+    soup = BeautifulSoup(html, 'lxml')
+
+    for img in soup.find('img', {'class': 'hide'}):
+        print("{0} : {1}".format("src картинки" ,img['src']) )
+
 
 def main():
     url = 'http://readmanga.me/'
 
-    url_manga = 'http://readmanga.me/tomo_chan_is_a_girl_/vol1/1'
+    url_manga = 'tomo_chan_is_a_girl_/vol1/1'
 
 
     #get_list_presentation(get_html(url + 'list/presentation/'))
-    get_manga(get_html(url_manga))
+    get_manga(get_html(url + url_manga))
 
 
 if __name__ == '__main__':
