c047569bd7b97a81a2b46d37d3ff007c195fd55a
mar4elkin <googlcraftlol@gmail.com>
2019-10-30 02:06:58 +0300

    feat(main): работа с картинками

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
+import re
+from urllib.parse import urljoin
+import ast
+import urllib.request
+import time
+from random import choice, uniform
+from time import sleep
+from urllib.request import urlretrieve
+import os
 
-def get_html(url):
-    r = requests.get(url)
+def get_html(url, useragent=None, proxy=None):
+    r = requests.get(url, headers=useragent, proxies=proxy)
     return r.text
 
 def get_list_presentation(html):
@@ -14,34 +23,69 @@ def get_list_presentation(html):
         print("{0}: {1} : {2}".format(i.a.div.text, href, j))
         j = j + 1 
 
-def get_manga(html):
+def get_manga_chapters(html):
     soup = BeautifulSoup(html, 'lxml')
 
     url_array = []
 
     for option in soup.find_all('option'):
-        print (('value: {}, text: {}').format(option['value'], option.text))
+        #print (('value: {}, text: {}').format(option['value'], option.text))
         url_array.append(option['value'][:-5])
 
-    for j in range(0, len(url_array)):
-        print(url_array[j])
+    return url_array[::-1]
 
+def get_url_images_from_chapter(url: str) -> list:
+    rs = requests.get(url)
 
-def get_picks(html):
-    soup = BeautifulSoup(html, 'lxml')
+    re_expr = r'init\(.*(\[\[.+\]\]).*\)'
 
-    for img in soup.find('img', {'class': 'hide'}):
-        print("{0} : {1}".format("src картинки" ,img['src']) )
+    match = re.search(re_expr, rs.text)
+    if not match:
+        raise Exception('Не получилось из страницы вытащить список картинок главы. '
+                        'Используемое регулярное выражение: ', re_expr)
+
+    json_text = match.group(1)
+    json_data = ast.literal_eval(json_text)
+
+    return [urljoin(data_url[1], data_url[0] + data_url[2]) for data_url in json_data]
+
+def download_images(urls, array):
+    
+    for i in range(0, len(array)):
+        normal_url = ''.join(urls)
+        current_path = os.getcwd()
+        name_of_new_path = "tomo_chan_is_a_girl_" + str(i)
+        os.mkdir(current_path + "\\" + name_of_new_path)
+        urlretrieve(normal_url, name_of_new_path)
+        #urllib.request.urlretrieve(urls, urls)
+    
 
 
 def main():
     url = 'http://readmanga.me/'
-
     url_manga = 'tomo_chan_is_a_girl_/vol1/1'
+    useragents = open('useragents.txt').read().split('\n')
+    proxies = open('proxies.txt').read().split('\n')
 
+    #get_list_presentation(get_html(url + 'list/presentation/', useragent, proxy))
+    #get_manga_chapters(get_html(url + url_manga, useragent, proxy))
 
-    #get_list_presentation(get_html(url + 'list/presentation/'))
-    get_manga(get_html(url + url_manga))
+    # Выгрузка картинок:
+
+    for i in range(40):
+        proxy = {'http': 'http://' + choice(proxies)}
+        useragent = {'User-Agent': choice(useragents)}
+        sleep(uniform(3, 6))
+
+        try:
+            chapters_array = get_manga_chapters(get_html(url + url_manga, useragent, proxy))
+        
+        except:
+            continue
+
+        for k in range(0, len(chapters_array)):
+                download_images(get_url_images_from_chapter(url + chapters_array[k]), chapters_array)
+    
 
 
 if __name__ == '__main__':
