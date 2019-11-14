71f00ad17f1f663fc8a90e1301568aa1015f11e3
mar4elkin <googlcraftlol@gmail.com>
2019-11-14 20:34:36 +0300

    feat(core): загрузчик классов

---
+++ LoadClass.php
@@ -0,0 +1,23 @@
+<?php
+  use Main\Factory as Factory;
+
+  spl_autoload_register(function($class) {
+   $path = __DIR__ . '/' . strtolower(str_replace('\\', '/', $class)) . '.php';
+    if (file_exists($path)) {
+    require_once $path;
+   } else {
+  echo $path;
+  }
+});
+
+define('MAIN', __DIR__);
+
+
+      $view       = (Factory::getClass('view'));    
+      $model      = (Factory::getClass('model'));
+      $controller = (Factory::getClass('controller'));  
+
+      $view->manga_detail('wolf_and_spice');
+
+
+ ?>
