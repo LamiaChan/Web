be64606ed93e6c3e06d5e725c7c3052ccaa4befd
mar4elkin <googlcraftlol@gmail.com>
2019-11-18 18:34:25 +0300

    feat(core/contr): Создание загрузки манги

--- main/controller.php
+++ main/controller.php
@@ -1,7 +1,39 @@
 <?php
 
    namespace Main;
+
    class Controller {
+
+      private $name;
+      private $description;
+      #private $img;
+
+      function __construct(){
+         $this->name = isset($_POST['manga_name']) ? $_POST['manga_name'] : null;
+         $this->description = isset($_POST['description']) ? $_POST['description'] : null;
+      }
+
+      public function AddManga(){
+         $manga_content = [];
+
+         if (empty($this->name)){
+            
+            throw new Exception("Empty Post not allowed. Manga name is empty");
+
+         } else {
+            array_push($manga_content, $this->name);
+         }
+
+         if (empty($this->description)){
+
+            throw new Exception("Empty Post not allowed. Description is empty");
+         
+         } else {
+            array_push($manga_content, $this->description);
+         }
+
+         
+      }
 
 }
 
