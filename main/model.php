edc991934d1f2e8d7fd86bc00f93e9720c49d792
mar4elkin <googlcraftlol@gmail.com>
2019-11-25 17:17:58 +0300

    feat(api): работа над api

--- main/model.php
+++ main/model.php
@@ -5,6 +5,12 @@
 
    class Model {
 
+      public function ApiPdo(){
+         $pdo = new PDO('mysql:host=localhost;dbname=lamia_chan', 'root', '',
+               array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+         return $pdo;
+      }
+
       public function CollectMangaUsingParser($manga_url){
          $pdo = new PDO('mysql:host=localhost;dbname=lamia_chan', 'root', '',
                      array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
