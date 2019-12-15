dd6a5249d3d6aaaa8b9f90fdd061c35b10218dc4
googlpasha <prostobux@yandex.ru>
2019-12-15 19:07:15 +0300

    vue project and bootstrap integrated

---
+++ frontend/Lamia_chan/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <MainHead></MainHead>
+    <button type="button" @click="getHashtags()">Грузи города</button>
+    <TestApi :testapi="testapi"> </TestApi>
+  </div>
+</template>
+
+<!-- INCLUDE CDN -->
+<script src="https://unpkg.com/axios@0.19.0/dist/axios.min.js"></script>
+
+<script>
+import TestApi from './components/TestApi.vue'
+import MainHead from './components/MainHead.vue'
+import axios from 'axios'
+
+
+export default {
+  name: 'app',
+    data(){
+    return {
+      todos: ['Ремонт', 'Уборка'],
+      testapi: [],
+      url: {
+          testapilink: 'http://localhost:8000/api/v1/manga/?format=json',
+      },
+    }
+  },
+  components: {
+    TestApi,
+    MainHead
+  },
+  methods: {
+    getHashtags(){
+        axios.get(this.url.testapilink).then((response) => {
+            this.testapi = response.data;
+            console.log("done")
+        });
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/Lamia_chan/src/components/TestApi.vue
@@ -0,0 +1,11 @@
+<template>
+    <ul>
+        <li v-for="(testapione,index) in testapi" :key="index">{{testapione}}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: ['testapi'],
+}
+</script>
