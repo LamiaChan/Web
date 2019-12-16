a0834f76330c7856c850ff4685a431a6047886e1
googlpasha <prostobux@yandex.ru>
2019-12-16 23:49:37 +0300

    detail.vue edit

---
+++ frontend/Lamia_chan/src/views/Detail.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <MainHead></MainHead>
+    </div>
+</template>
+
+<script>
+
+import MainHead from '../components/MainHead.vue'
+
+export default {
+    data(){
+        return {
+
+        }
+    },
+    components: {
+        MainHead,
+    },
+}
+</script>
+
+<style scoped>
+    body{
+        background: #5da130;
+    }
+</style>
