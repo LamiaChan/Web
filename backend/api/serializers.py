e12089333b37ae6fca0befc65562e254cd8fd5d9
mar4elkin <googlcraftlol@gmail.com>
2019-12-17 21:47:59 +0300

    переработ api

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,18 +1,36 @@
 from rest_framework import serializers
 from core.models import Manga, Page, Source, Chapter
 
-class MangaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Manga
-        fields = ('id', 'title', 'description', 'preview_image_url')
-
-
-class ChapterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Chapter
-        fields = ('id', 'title', 'manga_id')
-
 class PageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Page
-        fields = ('id', 'number', 'image', 'chapter_id')
+        fields = ['id', 'number', 'image', 'chapter_id']
+
+class ChapterSerializer(serializers.ModelSerializer):
+    page_set = serializers.SerializerMethodField()
+    class Meta:
+        model = Chapter
+        fields = ['id', 'title', 'manga_id', 'page_set']
+    
+    def get_page_set(self, instance):
+        page = instance.page_set.all()
+        return PageSerializer(page, many=True).data
+        
+
+class MangaSerializer(serializers.ModelSerializer):
+    chapter_set = serializers.SerializerMethodField()
+    class Meta:
+        model = Manga
+        fields = [
+            'id', 
+            'title', 
+            'description', 
+            'preview_image_url',
+            'chapter_set',
+
+        ]
+    
+    def get_chapter_set(self, instance):
+        chapter = instance.chapter_set.all()
+        return ChapterSerializer(chapter, many=True).data
+        
