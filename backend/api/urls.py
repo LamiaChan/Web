37bc59c972411ada70b2955769766174583f0d7a
mar4elkin <googlcraftlol@gmail.com>
2019-12-27 01:37:02 +0300

    testing api for javaapp

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,12 @@
 from rest_framework import routers
 from django.urls import include, path
 
-from .views import MangaViewSet, ChapterViewSet, PageViewSet, TagViewSet
+from .views import MangaViewSet, ChapterViewSet, PageViewSet, TagViewSet, JavaAppMangaViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'manga', MangaViewSet)
+router.register(r'jmanga', JavaAppMangaViewSet)
 router.register(r'chapter', ChapterViewSet)
 router.register(r'page', PageViewSet)
 router.register(r'tag', TagViewSet)
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .serializers import MangaSerializer, ChapterSerializer, PageSerializer, TagSerializer
 from core.models import Manga, Page, Source, Chapter, Tag
 
@@ -6,6 +7,17 @@ from core.models import Manga, Page, Source, Chapter, Tag
 class MangaViewSet(viewsets.ModelViewSet):
     queryset = Manga.objects.all()
     serializer_class = MangaSerializer
+
+class JavaAppMangaViewSet(viewsets.ModelViewSet):
+    queryset = Manga.objects.all()
+    serializer_class = MangaSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        serializer_data = serializer.data # get the default serialized representation
+        custom_data = {'content': serializer_data} # custom representation
+        return Response(custom_data)
 
 class ChapterViewSet(viewsets.ModelViewSet):
     queryset = Chapter.objects.all()
@@ -18,3 +30,4 @@ class PageViewSet(viewsets.ModelViewSet):
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+
