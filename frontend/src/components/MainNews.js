e8c0a521b6565857abb48e1bfcb1fbfae1329d05
googlpasha <prostobux@yandex.ru>
2020-04-14 00:34:10 +0300

    problem solved (i was stupid)

--- frontend/src/components/MainNews.js
+++ frontend/src/components/MainNews.js
@@ -6,21 +6,21 @@ import '../App.css'
 const NewsCard = (props) =>{
   const newsCards = []
   console.log(props.api)
-  // for (let i = 0; i < props.api.length; i++) {
-  //   newsCards.push(
-  //     <div key={props.api[i].id} className="col-md-3 centered">
-  //       <div className="mangaCard">
-  //         <img src={props.api[i].image} className="mangaCard__img" alt="manga"></img>
-  //         <div className="middle">
-  //           <div className="mangaName">{props.api[i].title}</div>
-  //         </div>
-  //       </div>
-  //     </div>
-   // )
-  //}
+  for (let i = 0; i < props.api.length; i++) {
+    newsCards.push(
+      <div key={props.api[i].id} className="col-md-3 centered">
+        <div className="newsCard">
+          <img src={props.api[i].image} className="newsCard__img" alt="manga"></img>
+          <div className="middle">
+            <div className="newsName">{props.api[i].title}</div>
+          </div>
+        </div>
+      </div>
+   )
+  }
   return(
     <div className="row">
-      {/* {newsCards} */}
+      {newsCards}
     </div>
   )
 }
@@ -31,7 +31,7 @@ export default class MainNews extends React.Component {
       <React.Fragment>
         <h3 className="componentTitle" style={{color:this.props.mainColor.textColor}}>Последние новости</h3>
         <div className="MainNews" style={{borderColor: this.props.mainColor.color, background: this.props.mainColor.smallBack, color:this.props.mainColor.textColor}}>
-          <NewsCard />
+          <NewsCard api={this.props.api} />
         </div>
       </React.Fragment>
     )
