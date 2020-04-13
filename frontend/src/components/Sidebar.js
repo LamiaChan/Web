3c6a4ef24aae6aed030f5eed0a79e6c1bfac529e
googlpasha <prostobux@yandex.ru>
2020-04-14 01:32:34 +0300

    ads done

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -6,15 +6,12 @@ import image from '../images/ads.jpg'
 export default class Sidebar extends React.Component {
   render(){
     return(
-    <React.Fragment>
-      <nav className="col-md-3 d-none d-md-block">
+      <div className="d-none d-md-block">
         <h3 className="sidebarTitle" style={{color:this.props.mainColor.textColor}}>Реклалма</h3>
         <div className="sidebar__content" style={{borderColor: this.props.mainColor.color, background: this.props.mainColor.smallBack, color:this.props.mainColor.textColor}}>
-          <h1>KEK</h1>
           <img className="ads__img" src={image}></img>
         </div>
-      </nav>
-    </React.Fragment>
+      </div>
     )
   }
 }
