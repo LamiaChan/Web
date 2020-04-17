cfd530838dfef05fb19948bba9d3eda4fa1b222c
googlpasha <prostobux@yandex.ru>
2020-04-17 16:03:31 +0300

    Tags done but need fixes

--- frontend/src/components/MainPage/MainGenres.js
+++ frontend/src/components/MainPage/MainGenres.js
@@ -18,21 +18,32 @@ const sortByLikes = (arr) =>{
 const MangaCardGenres = (props) =>{
   const mangaCards = [];
   const sortedApi = sortByLikes(props.api)
-  console.log(sortedApi)
   for (let i = 0; i < sortedApi.slice(0, 6).length; i++) {
-    mangaCards.push(
-      <div key={sortedApi[i].id} className="col-lg-2 centered">
-        <div className="mangaCard genreCard">
-          <img src={sortedApi[i].preview_image_url} className="mangaCard__img genreCard__img" alt="manga"></img>
-          <div className="likes">
-            <h4 className="likes__text"><i className="fa fa-heart"></i> {sortedApi[i].likes}</h4>
-          </div>
-          <div className="middle">
-            <div className="mangaName genreCard__img">{sortedApi[i].title} </div>
+    //Check manga tags and current tag
+    var findTag = false;
+    for (let j = 0; j < sortedApi[i].tags.length; j++) {
+      //Check all tags from current Manga
+      if (sortedApi[i].tags[j] === props.currentTag){
+        findTag = true;
+        break
+      }
+    }
+    //Show manga only with founded tag
+    if(findTag){
+      mangaCards.push(
+        <div key={sortedApi[i].id} className="col-lg-2 centered">
+          <div className="mangaCard genreCard">
+            <img src={sortedApi[i].preview_image_url} className="mangaCard__img genreCard__img" alt="manga"></img>
+            <div className="likes">
+              <h4 className="likes__text"><i className="fa fa-heart"></i> {sortedApi[i].likes}</h4>
+            </div>
+            <div className="middle">
+              <div className="mangaName genreCard__img">{sortedApi[i].title} </div>
+            </div>
           </div>
         </div>
-      </div>
-    )
+      )
+    }
   }
   return(
     <div className="row mr-0">
@@ -49,7 +60,7 @@ class CenresTabs extends React.Component{
   renderTags(){
     const tabs = [];
     const tapColor = {backgroundImage: "linear-gradient(to right, "+this.props.mainColor.color+", "+this.props.mainColor.color+" 50%, #000 50%)"};
-    for (let i = 0; i < this.props.apiTags.slice(0, 7).length; i++) {
+    for (let i = 0; i < this.props.apiTags.length; i++) {
       tabs.push(<li id={this.props.apiTags[i].id} className="tab" style={tapColor} onClick={this.props.changeCurrentTag}>{this.props.apiTags[i].title}</li>)
     }
     return tabs
@@ -71,13 +82,13 @@ export default class Genres extends React.Component {
   constructor(){
     super()
     this.state = {
-      currentTag: "Драма"
+      currentTag: 1
     }
     this.changeCurrentTag = this.changeCurrentTag.bind(this)
   }
   changeCurrentTag(event){
     this.setState({
-      currentTag : event.target.id
+      currentTag : parseInt(event.target.id)
     })
     console.log(this.state.currentTag)
   }
@@ -85,8 +96,8 @@ export default class Genres extends React.Component {
     return(
         <section className="genresSection" style={{background:this.props.mainColor.genres}}>
           <h3 className="componentTitle">Жанры</h3>
-            <CenresTabs mainColor={this.props.mainColor} apiTags={this.props.apiTags} changeCurrentTag={this.changeCurrentTag} currentTag={this.state.currentTag} />
-            <MangaCardGenres api={this.props.api} />
+            <CenresTabs mainColor={this.props.mainColor} apiTags={this.props.apiTags} changeCurrentTag={this.changeCurrentTag} />
+            <MangaCardGenres api={this.props.api} currentTag={this.state.currentTag} />
         </section>
     )
   }
