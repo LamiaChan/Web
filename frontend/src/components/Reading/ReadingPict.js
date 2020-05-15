d13bab07ac1a2a13651f90153ad296123e339d71
googlpasha <prostobux@yandex.ru>
2020-05-15 23:56:10 +0300

    scroll top then you turn a page

--- frontend/src/components/Reading/ReadingPict.js
+++ frontend/src/components/Reading/ReadingPict.js
@@ -30,6 +30,9 @@ class ReadingPage extends React.Component{
       default:
         break;
     }
+    //Scroll top
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     this.setState({
       currentPg: pgChang
     })
