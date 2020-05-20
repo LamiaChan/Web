59b524c5c802cee9ee4b66eaa903510998f83b49
googlpasha <prostobux@yandex.ru>
2020-05-21 00:18:16 +0300

    filter done (need styles)

--- frontend/src/components/Catalog/Catalog.js
+++ frontend/src/components/Catalog/Catalog.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Tags from './Tags'
 import Manga from './Manga'
+import CatalogFilter from './CatalogFilter'
 import './Catalog.css'
 
 //Redux-react connecter import
@@ -13,23 +14,36 @@ class Catalog extends React.Component{
     super()
     this.state = {
       manga: [],
-      tags: []
+      tags: [],
+      mangaFilter: '?likes=more'
     }
+    this.changeFilterPar = this.changeFilterPar.bind(this)
   }
 
   //API calls with Worker function
   async componentDidMount(){
-    await apiWorker(this.props.mangaLink).then(response => {this.setState({manga : response})})
+    console.log("Component rerendered")
+    //Add filter parameter to api link
+    await apiWorker(this.props.mangaLink + this.state.mangaFilter).then(response => {this.setState({manga : response})})
     await apiWorker(this.props.tagLink).then(response => {this.setState({tags : response})})
+  }
+  //Change filter parameters
+  async changeFilterPar(newPar){
+    await this.setState({
+      mangaFilter : newPar
+    })
+    await apiWorker(this.props.mangaLink + this.state.mangaFilter).then(response => {this.setState({manga : response})})
   }
 
   render(){
     return(
     <React.Fragment>
+      {this.state.mangaFilter}
       <div className="container-fluid  pr-0 pl-0">
         <Tags mainColor={this.props.mainColor} apiTags={this.state.tags} />
       </div>
       <div className="container-fluid">
+        <CatalogFilter changeFilterPar={this.changeFilterPar} />
         <Manga api={this.state.manga} />
       </div>
     </React.Fragment>
---
+++ frontend/src/components/Catalog/CatalogFilter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const CatalogFilters = (props) =>{
+
+    return(
+      <div className="row">
+        <div className="col-md-4">
+          <div className="dropdown">
+          <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Сортировать по
+          </button>
+          <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <span className="dropdown-item" onClick={()=>props.changeFilterPar("?likes=more")}>Возрасравстанию лайков</span>
+            <span className="dropdown-item" onClick={()=>props.changeFilterPar("?likes=less")}>Убыванию лайков</span>
+            <span cassName="dropdown-item" onClick={()=>props.changeFilterPar("?year_of_publish=more")}>Дате: от новых к старым</span>
+            <span className="dropdown-item" onClick={()=>props.changeFilterPar("?year_of_publish=less")}>Дате: от старых к товым</span>
+            <span className="dropdown-item" onClick={()=>props.changeFilterPar("?updated=more")}>Обновлениям: от недавних к старым</span>
+            <span className="dropdown-item" onClick={()=>props.changeFilterPar("?updated=less")}>Обновлению: от старых к недавним</span>
+          </div>
+         </div>
+        </div>
+      </div>
+    )
+
+}
+
+export default CatalogFilters
